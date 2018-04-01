5bac2994b226375452ef07bf245deeac95c52896
Muhammad Taimoor Sultani <sultani4work@gmail.com>
2018-04-01 14:15:38 +0500

    Pagination added in Customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,6 +3,6 @@ class CustomersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @customers = User.where(role: 1)
+    @customers = User.where(role: 1).order('created_at DESC').paginate(:page => params[:page], :per_page => 15)
   end
 end
